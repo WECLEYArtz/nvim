ee53bd46bef6640b5c3eee64e221485896e9a055
WECLEY <wecwhengithub7.anything491@slmail.me>
2025-11-06 05:17:07 +0100

    cleandup and disabled virtual text diagnose

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
 require("neowec.remap")
 require("neowec.options")
 require("neowec.lazy")
-require("neowec.plugin-remap")
--- lua/neowec/options.lua
+++ lua/neowec/options.lua
@@ -20,17 +20,11 @@ vim.opt.termguicolors = true
 
 vim.opt.clipboard = "unnamedplus"
 
--- vim.api.nvim_command('filetype indent off')
--- vim.opt.smartindent = false
-
 vim.opt.foldmethod = "manual"
--- vim.opt.foldexpr = "v:lua.vim.lsp.foldexpr()"
--- vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldlevel = 99
 vim.opt.foldcolumn = "1"
 vim.opt.foldtext = ""
 vim.opt.foldnestmax = 4
--- vim.opt.winborder = "single"
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
@@ -47,7 +41,7 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 	command = "silent! loadview",
 })
 vim.diagnostic.config({
-	virtual_lines = true,
+	virtual_lines = false,
 })
 
 -- better pasting
