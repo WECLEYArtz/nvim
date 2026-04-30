efd7aa8747cb293a4e8053a3cd47a02ff194ad16
WECLEY <wecwhengithub7.anything491@slmail.me>
2026-04-30 21:14:32 +0100

    Updated config: removed coloriser, changed treesitter branch, fixed telescope, fidget change

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,21 +1,8 @@
 return {
-	"nvim-treesitter/playground",
 	{
-		"nvim-treesitter/nvim-treesitter",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				sync_install = false,
-				auto_install = true,
-				highlight = {
-					enable = true,
-					disable = { "astro" },
-
-					-- additional_vim_regex_highlighting = true
-				},
-				indent = {
-					enable = false,
-				},
-			})
-		end,
-	},
+		'nvim-treesitter/nvim-treesitter',
+		branch = 'main',
+		lazy = false,
+		build = ':TSUpdate',
+	}
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,43 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		config = function()
+			local telescope = require("telescope")
+			telescope.setup({
+				defaults = {
+					mappings = {
+						n = {
+							-- Colemak change
+							["n"] = require("telescope.actions").move_selection_next,
+							["d"] = require("telescope.actions").delete_buffer,
+							["e"] = require("telescope.actions").move_selection_previous,
+
+							-- Nice easy exit
+							["q"] = function(prompt_bufnr)
+								require("telescope.actions").close(prompt_bufnr)
+							end,
+						},
+					},
+					layout_config = { preview_width = 0.6 },
+					path_display = { "smart" },
+				},
+				pickers = {
+					buffers = {
+						-- initial_mode = "normal",
+						sort_lastused = true,
+						sort_mru = true,
+						show_all_buffers = false,
+						path_display = { "filename_first" },
+					},
+					live_grep = {
+						path_display = { "shorten" },
+						--	search_dirs = { "./*" },
+						glob_pattern = {
+							"!node_modules",
+						},
+					},
+					colorscheme = { enable_preview = true },
+				},
+			})
 			local builtin = require("telescope.builtin")
 
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
@@ -12,43 +49,4 @@ return {
 			vim.keymap.set("n", "<leader>fc", builtin.colorscheme, { desc = "Telescope colorschemes" })
 		end,
 	},
-	require("telescope").setup({
-
-		defaults = {
-			mappings = {
-				n = {
-					-- Colemak change
-					["n"] = require("telescope.actions").move_selection_next,
-					["d"] = require("telescope.actions").delete_buffer,
-					["e"] = require("telescope.actions").move_selection_previous,
-
-					-- Nice easy exit
-					["q"] = function(prompt_bufnr)
-						require("telescope.actions").close(prompt_bufnr)
-					end,
-				},
-			},
-			layout_config = { preview_width = 0.6 },
-			path_display = { "smart" },
-		},
-
-		pickers = {
-			buffers = {
-				-- initial_mode = "normal",
-				sort_lastused = true,
-				sort_mru = true,
-				show_all_buffers = false,
-				path_display = { "filename_first" },
-			},
-			live_grep = {
-				path_display = { "shorten" },
-				--	search_dirs = { "./*" },
-				glob_pattern = {
-					"!node_modules",
-				},
-			},
-
-			colorscheme = { enable_preview = true },
-		},
-	}),
 }
